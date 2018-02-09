6e3fef1b493c149b09834d6d18bb2141a14b6081
Jeff <pzping@gmail.com>
2018-02-09 20:23:55 +0800

    fun

--- ddnspod/__main__.py
+++ ddnspod/__main__.py
@@ -2,42 +2,25 @@
 # -*- coding:utf-8 -*-
 
 import sys
+import logging
+from dnspodapi import main
 
-if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        ddns = DDnspod(getIDToken(sys.argv[1]),"pi")
-    else:
-        ddns = DDnspod(getIDToken(),"pi")
-    while True:
-        try:
-            ip = getip()
-            logging.info("IP: %s" % ip)
-            if current_ip != ip:
-                if ddns.updns(ip):
-                    current_ip = ip
-                else:
-                    logging.info("updnspod error")
-        except Exception as e:
-            print e
-            pass
-        time.sleep(60)
-
-from __future__ import absolute_import
-
-import os
-import sys
+logging.basicConfig(level=logging.INFO)
 
 # If we are running from a wheel, add the wheel to sys.path
 # This allows the usage python pip-*.whl/pip install pip-*.whl
+print __package__
 if __package__ == '':
     # __file__ is pip-*.whl/pip/__main__.py
     # first dirname call strips of '/__main__.py', second strips off '/pip'
     # Resulting path is the name of the wheel itself
     # Add that to sys.path so we can import pip
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
-
-import pip  # noqa
+    #path = os.path.dirname(os.path.dirname(__file__))
+    #sys.path.insert(0, path)
+    pass
 
 if __name__ == '__main__':
-    sys.exit(pip.main())
+    if len(sys.argv) >= 2:
+        main(sys.argv[1])
+    else:
+        main('./config.json')
--- ddnspod/dnspodapi.py
+++ ddnspod/dnspodapi.py
@@ -2,73 +2,73 @@
 # -*- coding:utf-8 -*-
 
 import sys
-import httplib
-import urllib
+#import httplib
+#import urllib
 import socket
 import requests
 import time
 import json
+import urlparse
 import logging
 
-logging.basicConfig(level=logging.INFO)
 
 # Use Token, check https://support.dnspod.cn/Kb/showarticle/tsid/227/
 
-current_ip = None
 
-class DDnspod(object):
-    def __init__(self, idtoken, sub_domain):
-        self.idtoken = idtoken
-        self.sub_domain = sub_domain
-        self.headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json", "User-Agent": "dnspod-python/0.01 (pzping@gmail.com)"}
-        self.domain_id = self.getdomainid()
-        self.record_id = self.getrecordid()
+def getdomainid(idtoken, domain):
+    params = dict(
+            login_token = idtoken,
+            format = 'json',
+            )
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/json', 'User-Agent': 'dnspod-python/0.01 (pzping@gmail.com)'}
+    resp = requests.post('https://dnsapi.cn/domain.list', data = params, headers = headers)
+    logging.debug(json.dumps(resp.json(), indent = 1))
+    for item in resp.json()['domains']:
+        if item['name'] == domain:
+            id = item['id']
+    #id = resp.json()['domains'][0]['id']
+    logging.info('Domain ID: %s'% id)
+    return id
 
-    def getdomainid(self):
-        params = dict(
-                login_token = self.idtoken,
-                format = "json",
-                )
-        resp = requests.post("https://dnsapi.cn/domain.list", data = params, headers = self.headers)
-        id = resp.json()["domains"][0]["id"]
-        logging.info("Domain ID: %s"% id)
-        return id
+def getrecordid(idtoken, domain_id, sub_domain):
+    params = dict(
+            login_token = idtoken,
+            sub_domain = sub_domain,
+            domain_id = domain_id,
+            format = 'json',
+            )
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/json', 'User-Agent': 'dnspod-python/0.01 (pzping@gmail.com)'}
+    resp = requests.post('https://dnsapi.cn/record.list', data = params, headers = headers)
+    logging.debug(json.dumps(resp.json(), indent = 1))
+    id = resp.json()['records'][0]['id']
+    logging.info('Record ID: %s'% id)
+    return id
 
-    def getrecordid(self):
-        params = dict(
-                login_token = self.idtoken,
-                sub_domain = self.sub_domain,
-                domain_id = self.domain_id,
-                format = "json",
-                )
-        resp = requests.post("https://dnsapi.cn/record.list", data = params, headers = self.headers)
-        id = resp.json()["records"][0]["id"]
-        logging.info("Record ID: %s"% id)
-        return id
+def updns(idtoken, sub_domain, domain_id, record_id, ip):
+    params = dict(
+            login_token = idtoken,
+            sub_domain = sub_domain,
+            domain_id = domain_id,
+            record_id = record_id,
+            format = 'json',
+            record_line='默认',
+            value = ip,
+            )
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/json', 'User-Agent': 'dnspod-python/0.01 (pzping@gmail.com)'}
+    resp = requests.post('https://dnsapi.cn/record.ddns', data = params, headers = headers)
+    logging.info('%d %s' % (resp.status_code,resp.reason))
+    print json.dumps(resp.json(), indent = 1)
+    return resp.status_code == 200
+    #conn = httplib.HTTPSConnection('dnsapi.cn')
+    #conn.request('POST', '/Record.Ddns', urllib.urlencode(params), headers)
 
-    def updns(self, ip):
-        params = dict(
-                login_token = self.idtoken,
-                sub_domain = self.sub_domain,
-                domain_id = self.domain_id,
-                record_id = self.record_id,
-                format = "json",
-                record_line="默认",
-                value = ip,
-                )
-        resp = requests.post("https://dnsapi.cn/record.ddns", data = params, headers = self.headers)
-        logging.info("%d %s" % (resp.status_code,resp.reason))
-        print json.dumps(resp.json(), indent = 1)
-        return resp.status_code == 200
-        #conn = httplib.HTTPSConnection("dnsapi.cn")
-        #conn.request("POST", "/Record.Ddns", urllib.urlencode(params), self.headers)
+    #response = conn.getresponse()
+    #print response.status, response.reason
+    #data = json.loads(response.read())
+    #print json.dumps(data, indent = 1)
+    #conn.close()
+    #return response.status == 200
 
-        #response = conn.getresponse()
-        #print response.status, response.reason
-        #data = json.loads(response.read())
-        #print json.dumps(data, indent = 1)
-        #conn.close()
-        #return response.status == 200
 
 def getip():
     sock = socket.create_connection(('ns1.dnspod.net', 6666), 20)
@@ -76,27 +76,36 @@ def getip():
     sock.close()
     return ip
 
-def getIDToken(filename = "./dnspodToken.json"):
-    with open(filename,"r") as jfile:
-        tken = json.load(jfile)
-        return "%s,%s" % (tken["ID"],tken["Token"])
+
+def main(filename):
+    current_ip = None
+
+    with open(filename,'r') as jfile:
+        dconf = json.load(jfile)
+
+    idtoken = '{id},{token}'.format(id = dconf['id'], token = dconf['token'])
+    domain_id = getdomainid(idtoken, dconf['domain'])
+    record_id = getrecordid(idtoken, domain_id, dconf['sub_domain'])
+
+    #while True:
+    #    try:
+    #        ip = getip()
+    #        logging.info('IP: %s' % ip)
+    #        #if current_ip != ip:
+    #        #    if updns(idtoken, dconf['sub_domain'], domain_id, record_id, ip):
+    #        #        current_ip = ip
+    #        #    else:
+    #        #        logging.info('updnspod error')
+    #    except Exception as e:
+    #        print e
+    #        pass
+    #    time.sleep(60)
+    pass
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
-        ddns = DDnspod(getIDToken(sys.argv[1]),"pi")
+        main(sys.argv[1])
     else:
-        ddns = DDnspod(getIDToken(),"pi")
-    while True:
-        try:
-            ip = getip()
-            logging.info("IP: %s" % ip)
-            if current_ip != ip:
-                if ddns.updns(ip):
-                    current_ip = ip
-                else:
-                    logging.info("updnspod error")
-        except Exception as e:
-            print e
-            pass
-        time.sleep(60)
-
+        main('./config.json')
